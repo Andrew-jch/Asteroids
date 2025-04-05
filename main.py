02ee0cffad8d9834500c4c4f8f251d044dc15d15
Andrew-jch <andrewhillss10@gmail.com>
2025-04-05 13:55:09 +0100

    Added player movement

--- main.py
+++ main.py
@@ -10,16 +10,29 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
              return
+            
+
         
         
         pygame.Surface.fill(screen, (0, 0, 0))
-        player.draw(screen)
+        updatable.update(dt)
+
+        for instance in drawable:
+           instance.draw(screen)
+
         pygame.display.flip()
 
 
